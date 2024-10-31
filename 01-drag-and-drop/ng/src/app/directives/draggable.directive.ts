705dba23307dc8f124184021f996df017429facc
Sandro Paetzold <sandro@paetzold.co.uk>
2024-10-31 20:28:05 +0000

    Formay draggable directive and clean up HTML structure

--- 01-drag-and-drop/ng/src/app/directives/draggable.directive.ts
+++ 01-drag-and-drop/ng/src/app/directives/draggable.directive.ts
@@ -8,38 +8,38 @@ export class DraggableDirective {
 
   private _offset: { x: number, y: number } = { x: 0, y: 0 };
 
-/**
- * Constructor
- * Initializes styles, attributes, and event listeners for the element
- * @param element Injected reference to the element this directive is attached to
- */
-constructor(private element: ElementRef) {
-  this.element.nativeElement.addEventListener('dragstart', this.dragStart.bind(this)); // Add event listener for dragstart
-  this.element.nativeElement.setAttribute('draggable', 'true'); // Make the element draggable
-  this.element.nativeElement.style.position = 'absolute'; // Set the position to absolute
+  /**
+   * Constructor
+   * Initializes styles, attributes, and event listeners for the element
+   * @param element Injected reference to the element this directive is attached to
+   */
+  constructor(private element: ElementRef) {
+    this.element.nativeElement.addEventListener('dragstart', this.dragStart.bind(this)); // Add event listener for dragstart
+    this.element.nativeElement.setAttribute('draggable', 'true'); // Make the element draggable
+    this.element.nativeElement.style.position = 'absolute'; // Set the position to absolute
 
-  document.addEventListener('dragover', this.dragOver.bind(this)); // Add event listener for dragover
-  document.addEventListener('drop', this.drop.bind(this)); // Add event listener for drop
-}
+    document.addEventListener('dragover', this.dragOver.bind(this)); // Add event listener for dragover
+    document.addEventListener('drop', this.drop.bind(this)); // Add event listener for drop
+  }
 
-/**
- * DragStart event handler
- * Calculates the offset of the mouse pointer from the top-left corner of the element for correct dropping
- * @param event DragEvent
- */
-private dragStart(event: DragEvent): void {
-  let elementPos = this.element.nativeElement.getBoundingClientRect(); // Get the position of the element
-  this._offset = { x: elementPos.left - event.clientX, y: elementPos.top - event.clientY }; // Calculate the offset
-}
+  /**
+   * DragStart event handler
+   * Calculates the offset of the mouse pointer from the top-left corner of the element for correct dropping
+   * @param event DragEvent
+   */
+  private dragStart(event: DragEvent): void {
+    let elementPos = this.element.nativeElement.getBoundingClientRect(); // Get the position of the element
+    this._offset = { x: elementPos.left - event.clientX, y: elementPos.top - event.clientY }; // Calculate the offset
+  }
 
-/**
- * DragOver event handler
- * Prevents the default behaviour of the event to allow dropping the element anywhere
- * @param event DragEvent
- */
-private dragOver(event: DragEvent): void {
-  event.preventDefault();
-}
+  /**
+   * DragOver event handler
+   * Prevents the default behaviour of the event to allow dropping the element anywhere
+   * @param event DragEvent
+   */
+  private dragOver(event: DragEvent): void {
+    event.preventDefault();
+  }
 
   /**
    * Drop event handler
