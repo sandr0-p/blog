a94ac63a4f0847c99eb72d5f81d42398f3f2f84d
sandr0-p <70605028+sandr0-p@users.noreply.github.com>
2024-11-03 22:37:11 +0000

    Snapping (#3)
    * Rename folders
    
    * Copy project
    
    * Enhance drag-and-drop functionality with improved positioning and ghost image handling

---
+++ 03-drag-drop-engage/ng/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, signal, Signal, ViewChild, viewChild, WritableSignal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { DraggableDirective } from './directives/draggable.directive';
+import { GridDirective } from './directives/grid.directive';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, DraggableDirective, GridDirective],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'drag-and-drop';
+}
