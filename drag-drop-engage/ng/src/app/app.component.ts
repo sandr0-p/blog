04d4e427184fb7688bf893b443b7b367f8c66db1
Sandro Paetzold <sandro@paetzold.co.uk>
2024-11-10 16:01:37 +0000

    Add Angular logo and implement grid layout for drag-and-drop feature

--- drag-drop-engage/ng/src/app/app.component.ts
+++ drag-drop-engage/ng/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DraggableDirective } from './directives/draggable.directive';
+import { GridDirective } from './directives/grid.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DraggableDirective],
+  imports: [RouterOutlet, DraggableDirective, GridDirective],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
